d289130a93e2be2a5c6425ba1fe4f6796d677b07
DHurt <DevenHurt@gmail.com>
2015-12-11 20:58:41 -0500

    Added welcome button

--- Project Blacksky/Project BlackSky/DetailViewController.swift
+++ Project Blacksky/Project BlackSky/DetailViewController.swift
@@ -32,6 +32,14 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        
+        let getStarted = UIButton()
+        getStarted.setTitle("Get Started", forState: .Normal)
+        getStarted.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        getStarted.frame = CGRectMake((screenSize.width-120)/2, screenSize.width/2, 120, 30) // X, Y, width, height
+        getStarted.addTarget(self, action: "pressed:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(getStarted)
         self.configureView()
     }
 
@@ -39,7 +47,22 @@ class DetailViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func pressed(sender: UIButton!) {
+        let alertController = UIAlertController(title: "Alert", message: "Is this working?", preferredStyle: .Alert)
+        // Initialize Actions
+        let yesAction = UIAlertAction(title: "Yes", style: .Default) { (action) -> Void in
+            print("Working.")
+        }
+        let noAction = UIAlertAction(title: "No", style: .Default) { (action) -> Void in
+            print("You say it isn't, but it is.")
+        }
+        // Add Actions
+        alertController.addAction(yesAction)
+        alertController.addAction(noAction)
+        // Present Alert Controller
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
 
 }
 
