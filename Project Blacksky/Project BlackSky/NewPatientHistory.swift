26b21325d8a7d077f6e1407005d5f56b122e816c
DHurt <DevenHurt@gmail.com>
2016-01-23 01:10:44 -0500

    Just commit from today's work

--- Project Blacksky/Project BlackSky/NewPatientHistory.swift
+++ Project Blacksky/Project BlackSky/NewPatientHistory.swift
@@ -17,6 +17,7 @@ class NewPatientHistory: UIViewController {
     var detailItem: AnyObject? {
         didSet {
             // Update the view.
+
             
             self.configureView()
         }
@@ -29,21 +30,23 @@ class NewPatientHistory: UIViewController {
     
     //Smoker? Current Meds? Current illnesses? Family history of illness?
     
-    let yesLabel:UILabel = UILabel()
-    let noLabel:UILabel = UILabel()
     let greetingLabel:UILabel = UILabel()
     let smokerStatusLabel:UILabel = UILabel()
     let smokerBox:CheckBox = CheckBox()
-    let nonSmokerBox:CheckBox = CheckBox()
+    let smokerButton:UIButton = UIButton()
+    let smokerField:UITextField = UITextField()
     let drinkLabel:UILabel = UILabel()
     let drinkBox:CheckBox = CheckBox()
-    let nonDrinkerBox:CheckBox = CheckBox()
+    let drinkButton:UIButton = UIButton()
+    let drinkerField:UITextField = UITextField()
     let currentMedsLabel:UILabel = UILabel()
     let currentMedsBox:CheckBox = CheckBox()
-    let noCurrentMedsBox:CheckBox = CheckBox()
+    let currentMedsButton:UIButton = UIButton()
+    let medsField:UITextField = UITextField()
     let currentConditionsLabel:UILabel = UILabel()
+    let currentConditionsButton:UIButton = UIButton()
     let currentConditionsBox:CheckBox = CheckBox()
-    let noCurrentConditionsBox:CheckBox = CheckBox()
+    let conditionsField:UITextField = UITextField()
     let nextButton:UIButton = UIButton()
     
     override func viewDidLoad() {
@@ -59,42 +62,53 @@ class NewPatientHistory: UIViewController {
         nextButton.titleLabel?.font = UIFont(name: (nextButton.titleLabel?.font?.fontName)!, size: 30)
         nextButton.addTarget(self, action: "nextClicked:", forControlEvents: .TouchUpInside)
         
-        creationFunctions.makeLabel(greetingLabel, name: "Please answer the following questions by touching the checkbox:", textColor: UIColor.blackColor(), alignment: NSTextAlignment.Left, frame: CGRectMake(screenSize.width/6, screenSize.height/15, 800, 40), page: self)
+        creationFunctions.makeLabel(greetingLabel, name: "Please answer the following questions by touching the checkbox:", textColor: UIColor.blackColor(), alignment: NSTextAlignment.Left, frame: CGRectMake(screenSize.width/10, screenSize.height/15, 800, 40), page: self)
         greetingLabel.font = UIFont(name: (greetingLabel.font?.fontName)!, size: 25)
         greetingLabel.sizeToFit()
-        
-        creationFunctions.makeLabel(yesLabel, name:"Yes", textColor: UIColor.blackColor(), alignment: NSTextAlignment.Left, frame: CGRectMake(8 * screenSize.width/10 , screenSize.height*3/20, 50, 30), page: self)
-        yesLabel.font = UIFont(name: (yesLabel.font?.fontName)!, size:30)
-        yesLabel.sizeToFit()
-        creationFunctions.makeLabel(noLabel, name:"No", textColor: UIColor.blackColor(), alignment: NSTextAlignment.Left, frame: CGRectMake(9 * screenSize.width/10 , screenSize.height*3/20, 50, 30), page: self)
-        noLabel.font = UIFont(name: (noLabel.font?.fontName)!, size:30)
-        noLabel.sizeToFit()
         
         creationFunctions.makeLabel(smokerStatusLabel, name: "Do they regularly smoke?", textColor: UIColor.blackColor(), alignment: NSTextAlignment.Left, frame: CGRectMake(screenSize.width/20, 2 * screenSize.height/10, 500, 30), page: self)
         smokerStatusLabel.font = UIFont(name: (smokerStatusLabel.font?.fontName)!, size:30)
         smokerStatusLabel.sizeToFit()
         creationFunctions.makeCheckBox(smokerBox, frame: CGRectMake(8 * screenSize.width/10 , screenSize.height*2/10, 50, 50), page: self)
-        creationFunctions.makeCheckBox(nonSmokerBox, frame: CGRectMake(9 * screenSize.width/10 , screenSize.height*2/10, 50, 50), page: self)
+        creationFunctions.makeButton(smokerButton, name: "", titleColor: UIColor.clearColor(), location: smokerBox.frame, page: self)
+        smokerButton.addTarget(self, action: "doesSmoke:", forControlEvents: .TouchUpInside)
+        
         
         creationFunctions.makeLabel(drinkLabel, name: "Do they regularly consume alcohol?", textColor: UIColor.blackColor(), alignment: NSTextAlignment.Left, frame: CGRectMake(0.5 * screenSize.width/10, 3.5 * screenSize.height/10, 500, 35), page: self)
         drinkLabel.font = UIFont(name: (drinkLabel.font?.fontName)!, size:30)
         drinkLabel.sizeToFit()
         creationFunctions.makeCheckBox(drinkBox, frame: CGRectMake(8 * screenSize.width/10 , screenSize.height*3.5/10, 50, 50), page: self)
-        creationFunctions.makeCheckBox(nonDrinkerBox, frame: CGRectMake(9 * screenSize.width/10 , screenSize.height*3.5/10, 50, 50), page: self)
-        
+        creationFunctions.makeButton(drinkButton, name: "", titleColor: UIColor.clearColor(), location: drinkBox.frame, page: self)
+        drinkButton.addTarget(self, action: "doesDrink:", forControlEvents: .TouchUpInside)
+
         creationFunctions.makeLabel(currentMedsLabel, name: "Do they regularly take any medications?", textColor: UIColor.blackColor(), alignment: NSTextAlignment.Left, frame: CGRectMake(screenSize.width/20, 5 * screenSize.height/10, 500, 35), page: self)
         currentMedsLabel.font = UIFont(name: (currentMedsLabel.font?.fontName)!, size:30)
         creationFunctions.makeCheckBox(currentMedsBox, frame: CGRectMake(8 * screenSize.width/10 , screenSize.height*5/10, 50, 50), page: self)
         currentMedsLabel.sizeToFit()
-        creationFunctions.makeCheckBox(noCurrentMedsBox, frame: CGRectMake(9 * screenSize.width/10 , screenSize.height*5/10, 50, 50), page: self)
+        creationFunctions.makeButton(currentMedsButton, name: "", titleColor: UIColor.clearColor(), location: currentMedsBox.frame, page: self)
         
         creationFunctions.makeLabel(currentConditionsLabel, name: "Do they have any persisting medical conditions?", textColor: UIColor.blackColor(), alignment: NSTextAlignment.Left, frame: CGRectMake(screenSize.width/20, 6.5 * screenSize.height/10, 500, 35), page: self)
         currentConditionsLabel.font = UIFont(name: (currentConditionsLabel.font?.fontName)!, size:30)
         currentConditionsLabel.sizeToFit()
         creationFunctions.makeCheckBox(currentConditionsBox, frame: CGRectMake(8 * screenSize.width/10 , screenSize.height*6.5/10, 50, 50), page: self)
-        creationFunctions.makeCheckBox(noCurrentConditionsBox, frame: CGRectMake(9 * screenSize.width/10 , screenSize.height*6.5/10, 50, 50), page: self)
-        
+        creationFunctions.makeButton(currentConditionsButton, name: "", titleColor: UIColor.clearColor(), location: currentConditionsBox.frame, page: self)
         self.configureView()
+    }
+    
+    func doesSmoke(sender: UIButton){
+        if smokerBox.isChecked{
+            smokerBox.isChecked = !smokerBox.isChecked
+            creationFunctions.makeTextField(smokerField, backgroundColor: UIColor.lightGrayColor(), frame: CGRectMake(8 * screenSize.width/10 , screenSize.height * 2.5/10, 50, 50), page: self)
+            smokerField.placeholder = "How much/how often?"
+        } else{
+            smokerBox.isChecked = !smokerBox.isChecked
+            smokerField.hidden = true
+            smokerField.enabled = false
+        }
+    }
+    
+    func doesDrink(Sender: UIButton){
+        
     }
     
     func nextClicked(sender: UIButton){
