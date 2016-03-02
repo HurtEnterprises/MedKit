2fc4b091e6fc9fffe566138615136030dfd14020
DHurt <DevenHurt@gmail.com>
2016-03-02 03:09:05 -0500

    Beginnings of redisigned menu UI

--- Project Blacksky/Project BlackSky/MainMenu.swift
+++ Project Blacksky/Project BlackSky/MainMenu.swift
@@ -17,85 +17,58 @@ let myAppointmentsPage: MyApointments = MyApointments()
 let NewPatientPage: NewPatient = NewPatient()
 
 
-class MainMenu: UIViewController,UITableViewDelegate, UITableViewDataSource {
+class MainMenu: UIViewController {
 
-
-        var tableView: UITableView  =   UITableView()
-        
-        var items: [String] = ["My Appointments", "My Patients", "New Patient", "Logout"]
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            self.view.backgroundColor = UIColor.whiteColor()
-            
-            tableView.layoutMargins = UIEdgeInsetsZero
-            tableView.separatorInset = UIEdgeInsetsZero
-            
-            // Makes Navigation Bar
-            let navigationBar = UINavigationBar(frame: CGRectMake(0, 20, self.view.frame.size.width, 50)) // Offset by 20 pixels vertically to take the status bar into account
-
-            creationFunctions.makeNavigationBar(navigationBar, barTitle: "Main Menu", color: UIColor.grayColor(), forwardButton: false, backButton: false, page: self)
-            
-            // Makes Table
-            
-            tableView.frame         =   CGRectMake(0, 71, screenSize.width, screenSize.height);
-            tableView.delegate      =   self
-            tableView.dataSource    =   self
-            
-            tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
-            tableView.rowHeight = UITableViewAutomaticDimension
-            self.tableView.estimatedRowHeight = 200
-            
-            tableView.alwaysBounceVertical = false;
-            self.tableView.separatorStyle = UITableViewCellSeparatorStyle.SingleLine
-            self.tableView.separatorColor = UIColor.whiteColor()
-                        
-            self.view.addSubview(tableView)
-        }
-        
-        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return self.items.count
-        }
-        
-        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            
-            let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
-            
-            cell.textLabel?.text = self.items[indexPath.row]
-            
-            cell.backgroundColor = UIColor.lightGrayColor()
-            
-            cell.layoutMargins = UIEdgeInsetsZero
-            
-            return cell
-            
-        }
-    
-    func tableView(tableView: UITableView,
-        heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-            if indexPath.row == 3 {
-                return height*24/100 //Whatever fits your need for that cell
-            } else {
-                return height*24/100 // other cell height
-            }
+    func configureView() {
+        // Update the user interface for the detail item.
+        self.view.backgroundColor = UIColor.cyanColor()
     }
+    // Initializes labels and buttons
+    let mainMenuLabel:UILabel = UILabel()
+    let nextPatientLabel:UILabel = UILabel()
+    let appointmentTime:UILabel = UILabel()
+    
+    let startVisitButton:UIButton = UIButton()
+    let scheduleButton:UIButton = UIButton()
+    let myPatientsButton:UIButton = UIButton()
+    let chatButton:UIButton = UIButton()
+    let logoutButton:UIButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blueColor()
+
+            // Makes Navigation Bar
+        let navigationBar = UINavigationBar(frame: CGRectMake(0, 20, self.view.frame.size.width, 50)) // Offset by 20 pixels vertically to take the status bar into account
+        navigationBar.backgroundColor = UIColor.grayColor()
+        // Create a navigation item with a title
+        let navigationItem = UINavigationItem()
+        navigationItem.title = ""
+                   // Create two buttons for the navigation item
+        let rightButton =  UIBarButtonItem(title: "Get Dr's Name | My Account", style:   UIBarButtonItemStyle.Plain, target: self, action: "nextClickedClicked:")
+            // Create two buttons for the navigation item
+            navigationItem.rightBarButtonItem = rightButton
         
-        func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-            if (indexPath.row == 0){
-                myAppointmentsClicked()
-            } else if (indexPath.row == 1){
-                myPatientsClicked()
-            } else if (indexPath.row == 2){
-                newPatientClicked()
-            } else if (indexPath.row == 3){
-                logoutClicked()
-            }else{
-                print("Invalid selection.")
-            }
-            print("You selected cell #\(indexPath.row)!")
-        }
+        // Assign the navigation item to the navigation bar
+        navigationBar.items = [navigationItem]
+        //Adds the bar to the view
+        self.view.addSubview(navigationBar)
         
+        creationFunctions.makeLabel(mainMenuLabel, name: "Main Menu", textColor: UIColor.whiteColor(), alignment: NSTextAlignment.Center, frame: CGRectMake(width/2, (height-50)/8, 200, 50), page: self)
+        mainMenuLabel.sizeToFit()
+        
+        creationFunctions.makeLabel(nextPatientLabel, name: "My Next Patient Is:", textColor: UIColor.whiteColor(), alignment: NSTextAlignment.Center, frame: CGRectMake(width/2, (height-50)/4 , 200, 50), page: self)
+        
+        creationFunctions.makeLabel(appointmentTime, name: "At + time", textColor: UIColor.whiteColor(), alignment: NSTextAlignment.Center, frame: CGRectMake(width/2, (height-50)*3/8, 200, 50), page: self)
+        
+        creationFunctions.makeButton(startVisitButton, name: "*Patient Name*", titleColor: UIColor.whiteColor(), location: CGRectMake(width/2,(height-50)/2 , 200, 50), page: self)
+        
+        creationFunctions.makeButton(scheduleButton, name: "My Schedule", titleColor: UIColor.whiteColor(), location: CGRectMake(width/2, (height-50)*5/8, 200, 50), page: self)
+        
+        creationFunctions.makeButton(chatButton, name: "Chat", titleColor: UIColor.whiteColor(), location: CGRectMake(width/2, (height-50)*3/4, 200, 50), page: self)
+        
+        creationFunctions.makeButton(logoutButton, name: "Logout", titleColor: UIColor.whiteColor(), location: CGRectMake(width/2, (height-50)*7/8, 200, 50), page: self)
+    }
         override func didReceiveMemoryWarning() {
             super.didReceiveMemoryWarning()
             // Dispose of any resources that can be recreated.
@@ -107,6 +80,10 @@ class MainMenu: UIViewController,UITableViewDelegate, UITableViewDataSource {
         }
         self.dismissViewControllerAnimated(true, completion: nil)
         //TODO: @Mehrab add some sort of logout function or we can make it so the user is logged out whenever they hit the login page?
+    }
+    
+    func backClicked(sender: UIBarButtonItem!){
+        self.dismissViewControllerAnimated(true, completion: {});
     }
     
     func myPatientsClicked(){
