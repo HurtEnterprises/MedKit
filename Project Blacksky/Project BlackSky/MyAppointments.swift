2a7159d690841f801e25a520a53c5deffe988e06
DHurt <DevenHurt@gmail.com>
2017-01-12 22:28:53 -0500

    Bug Fix to stop app from crashing when click my schedule button

--- Project Blacksky/Project BlackSky/MyAppointments.swift
+++ Project Blacksky/Project BlackSky/MyAppointments.swift
@@ -34,7 +34,7 @@ class MyApointments: UIViewController {
         // Makes Navigation Bar
         let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 20, width: self.view.frame.size.width, height: 50)) // Offset by 20 pixels vertically to take the status bar into account
         
-        creationFunctions.makeNavigationBar(navigationBar, barTitle: "My Appointments", forwardButton: false, backButton: true, page: self)
+        creationFunctions.makeNavigationBar(navigationBar, barTitle: "My Appointments", forwardButton: false, backButton: false, page: self)
         
         
         self.configureView()
